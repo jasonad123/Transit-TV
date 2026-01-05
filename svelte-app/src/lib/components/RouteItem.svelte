d1a3baacc6630b04abdafe6fb05409ad9345072f
Jason Adle <74881573+jasonad123@users.noreply.github.com>
2026-01-05 17:53:09 -0500

    feat: implement itinerary re-merging

--- svelte-app/src/lib/components/RouteItem.svelte
+++ svelte-app/src/lib/components/RouteItem.svelte
@@ -1029,7 +1029,7 @@
 								class="destination-text"
 								class:scrolling={overflowingDestinations.has(index)}
 								use:bindDestinationElement={index}
-								>{dir.merged_headsign || dir.direction_headsign || 'Unknown destination'}</span
+								>{dir.merged_headsign || dir.direction_headsign ||'Unknown destination'}</span
 							>
 						</h3>
 
